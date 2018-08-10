d3b52454d4f9a7c6ea0f546e5fe8d879583ced31
Sheean Spoel <s.j.j.spoel@uu.nl>
2018-08-10 15:19:31 +0200

    Support collecting directly from a directory path

--- corpus2alpino/collectors/filesystem.py
+++ corpus2alpino/collectors/filesystem.py
@@ -2,7 +2,7 @@
 import glob
 from typing import Iterable, List
 from pathlib import Path
-from os import path
+from os import listdir, path
 
 from corpus2alpino.abstracts import Collector
 from corpus2alpino.models import CollectedFile
@@ -11,19 +11,37 @@ from corpus2alpino.models import CollectedFile
 class FilesystemCollector(Collector):
     position = 0
 
+    def __clear_pattern(self, filepath):
+        realpath = filepath.split('*')[0]
+        if path.isdir(realpath):
+            return realpath
+        else:
+            return path.split(realpath)[0]
+
     def __init__(self, filepaths: List[str]) -> None:
-        self.common = path.commonpath(filepaths)
+        # Only determine common directory up to the first pattern
+        self.common = path.commonpath(
+            self.__clear_pattern(filepath) for filepath in filepaths)
         self.filepaths = filepaths
         self.total = len(filepaths)
 
     def read(self) -> Iterable[CollectedFile]:
         self.position = 0
-        for filepath in self.filepaths:
-            globbed = glob.glob(path.expanduser(filepath), recursive=True)
+        return self.yield_files(self.filepaths)
+
+    def yield_files(self, filepaths):
+        for filepath in filepaths:
+            globbed = glob.glob(filepath, recursive=True)
             self.total += len(globbed) - 1
 
             for match in globbed:
-                (relpath, filename) = path.split(path.relpath(match, self.common))
+                if path.isdir(match):
+                    yield from self.yield_files(
+                        path.join(match, file) for file in listdir(match))
+                    continue
+
+                (relpath, filename) = path.split(
+                    path.relpath(match, self.common))
                 # TODO: mime type?
                 with open(match) as file:
                     yield CollectedFile(relpath, filename, '', file.read())

8d9ce4aaf25a595d9d975fbd4df2115f0f9b0015
Sheean Spoel <s.j.j.spoel@uu.nl>
2019-12-12 12:20:34 +0100

    Parse chat line tiers, allow utterance id override (#11)
    Parse chat line tiers, allow utterance id override

--- corpus2alpino/readers/chat.py
+++ corpus2alpino/readers/chat.py
@@ -2,14 +2,17 @@
 """
 Module for reading CHAT cha files to parsable utterances.
 """
-from typing import Dict, Iterable, List, Tuple
-from chamd import ChatReader as ChatParser
+from typing import cast, Dict, Iterable, List, Tuple
+from chamd import ChatReader as ChatParser, ChatLine, ChatTier
 
 import os
 import re
 
 from corpus2alpino.abstracts import Reader
 from corpus2alpino.models import CollectedFile, Document, MetadataValue, Utterance
+
+MANUAL_IDS = ['xsid', 'xuid']
+UTTERANCE_NUMBER_ID = 'uttno'
 
 class ChatReader(Reader):
     """
@@ -18,23 +21,44 @@ class ChatReader(Reader):
 
     def read(self, collected_file: CollectedFile) -> Iterable[Document]:
         self.reader = ChatParser()
-        chat = self.reader.read_string(collected_file.content, collected_file.filename)
+        chat = self.reader.read_string(
+            collected_file.content, collected_file.filename)
         yield Document(collected_file,
-            list(self.parse_utterances(chat.lines)),
-            self.parse_metadata(chat.metadata))
+                       list(self.parse_utterances(chat.lines)),
+                       self.parse_metadata(chat.metadata))
 
-    def parse_utterances(self, chat_lines):
+    def parse_utterances(self, chat_lines: List[ChatLine]):
+        number = 0
         for line in chat_lines:
-            yield Utterance(line.text,
-                 str(line.uttid),
-                 self.parse_metadata(line.metadata),
-                 int(line.metadata['uttstartlineno'].text))
+            number += 1  # start numbering utterances from 1
+            for id_override_key in MANUAL_IDS:
+                try:
+                    line.uttid = line.tiers[id_override_key].text
+                    line.metadata['uttid'].text = line.uttid
+                    break
+                except KeyError:
+                    pass
 
-    def parse_metadata(self, metadata):
-        parsed = {}
+            yield Utterance(line.text,
+                            str(line.uttid),
+                            {
+                                **self.parse_metadata(line.metadata),
+                                **self.parse_tiers(line.tiers),
+                                UTTERANCE_NUMBER_ID: MetadataValue(str(number), 'int')
+                            },
+                            int(line.metadata['uttstartlineno'].text))
+
+    def parse_metadata(self, metadata) -> Dict[str, MetadataValue]:
+        parsed = cast(Dict[str, MetadataValue], {})
         for (key, item) in metadata.items():
             parsed[key] = MetadataValue(item.text, item.value_type)
 
+        return parsed
+
+    def parse_tiers(self, tiers: Dict[str, ChatTier]) -> Dict[str, MetadataValue]:
+        parsed = cast(Dict[str, MetadataValue], {})
+        for tier in tiers.values():
+            parsed[tier.name] = MetadataValue(tier.text)
         return parsed
 
     def test_file(self, file: CollectedFile):

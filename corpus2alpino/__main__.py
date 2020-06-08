2904447856977505a480eef91d32ef86e73ecda0
Sheean Spoel <s.j.j.spoel@uu.nl>
2020-06-08 14:15:17 +0200

    Support older Alpino (execs), prefix duplicates

--- corpus2alpino/__main__.py
+++ corpus2alpino/__main__.py
@@ -33,6 +33,9 @@ def main(args=None):
             '-s', '--server', metavar='SERVER', type=str,
             help='host:port of Alpino server')
         parser.add_argument(
+            '-sp', '--subprocess', metavar='SUBPROCESS', type=str, nargs='+',
+            help='Path to Alpino executable followed by the arguments (e.g. /opt/Alpino -veryfast debug=0), MUST BE GIVEN AS LAST ARGUMENT')
+        parser.add_argument(
             '-o', '--output_path', metavar='OUTPUT', type=str,
             help='Output path')
         parser.add_argument(
@@ -43,13 +46,29 @@ def main(args=None):
 
         parser.set_defaults(split_treebanks=False)
 
+        # passthrough the subprocess arguments
+        subprocess = None
+        for i, arg in enumerate(args):
+            if arg == "-sp" or arg == "--subprocess":
+                subprocess = args[i+1:]
+                args = args[:i]
+                break
+
         options = parser.parse_args(args)
 
         collector = FilesystemCollector(options.file_names)
         converter = Converter(collector)
-        if options.server != None:
-            [host, port] = options.server.split(":")
-            converter.annotators.append(AlpinoAnnotator(host, int(port)))
+        if options.server != None or subprocess != None:
+            if options.server != None:
+                [host, port] = options.server.split(":")
+                converter.annotators.append(AlpinoAnnotator(host, int(port)))
+            else:
+                executable = subprocess[0]
+                arguments = [arg for arg in subprocess[1:]] \
+                    + subprocess[2:]
+                converter.annotators.append(
+                    AlpinoAnnotator(executable, arguments))
+
             converter.writer = LassyWriter(not options.split_treebanks)
 
         if options.output_path != None:

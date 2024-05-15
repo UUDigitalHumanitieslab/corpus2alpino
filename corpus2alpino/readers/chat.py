b9ac270dae6bc36ef247ffda658aa9dc6c08d286
Jelte van Boheemen <j.vanboheemen@uu.nl>
2024-05-15 15:10:41 +0200

    Remove manual override keys for uttid in ChatReader
    Resolves #30

--- corpus2alpino/readers/chat.py
+++ corpus2alpino/readers/chat.py
@@ -2,17 +2,15 @@
 """
 Module for reading CHAT cha files to parsable utterances.
 """
-from typing import cast, Dict, Iterable, List, Tuple
+from typing import cast, Dict, Iterable, List
 from chamd import ChatReader as ChatParser, ChatLine, ChatTier
 
-import os
-import re
 
 from corpus2alpino.abstracts import Reader
 from corpus2alpino.models import CollectedFile, Document, MetadataValue, Utterance
 
-MANUAL_IDS = ['xsid', 'xuid']
 UTTERANCE_NUMBER_ID = 'uttno'
+
 
 class ChatReader(Reader):
     """
@@ -31,13 +29,6 @@ class ChatReader(Reader):
         number = 0
         for line in chat_lines:
             number += 1  # start numbering utterances from 1
-            for id_override_key in MANUAL_IDS:
-                try:
-                    line.uttid = line.tiers[id_override_key].text
-                    line.metadata['uttid'].text = line.uttid
-                    break
-                except KeyError:
-                    pass
 
             yield Utterance(line.text,
                             str(line.uttid),
